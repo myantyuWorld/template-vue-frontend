86a356b6eeb6b5c03dbec2bebdf931a25f15de3a
myantyuWorld <yuichi.oba@sorich.jp>
2022-12-26 23:26:32 +0900

    add cssframework(tailwind)

--- vite_fast/src/main.ts
+++ vite_fast/src/main.ts
@@ -5,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import './assets/styles/main.scss'
+import './assets/styles/tailwind.scss'
 
 const app = createApp(App)
 

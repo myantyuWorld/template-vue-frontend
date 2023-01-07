ee5b9afda09f51d9f25fdff0b28b22cb0470fd17
myantyuWorld <nepinepi984@gmail.com>
2023-01-07 10:11:45 +0900

    django rest framework初期設定

--- api/django_api/django_api/urls.py
+++ api/django_api/django_api/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.contrib import admin
+from webapi.urls import router as webapi_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(webapi_router.urls)),
 ]

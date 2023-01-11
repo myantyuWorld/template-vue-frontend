2ab752b052892dab4cb6928d1e8475dfb1762927
myantyuWorld <nepinepi984@gmail.com>
2023-01-12 00:07:16 +0900

    Modify django-api/Dockerfile

--- api/django_api/django_api/settings.py
+++ api/django_api/django_api/settings.py
@@ -81,15 +81,21 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#          'NAME': 'postgres',
+#          'USER': 'postgres',
+#          'PASSWORD': 'password',
+#          'HOST': 'db',
+#          'PORT': '5432',
+#      }
+# }
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'postgres',
-         'USER': 'postgres',
-         'PASSWORD': 'password',
-         'HOST': 'db',
-         'PORT': '5432',
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 

58624abb1614e1fb7bfa0df92d60cfaa1d6ad234
myantyuworld <nepinepi984@gmail.com>
2023-01-23 16:07:40 +0900

    Delete api/django-api | django-copilotを正とするため削除

--- api/django_api/webapi/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from webapi.models import *
-
-admin.site.register(Member)
-
-# Register your models here.
--- api/django_api/webapi/urls.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import routers
-from webapi.views import *
-
-router = routers.DefaultRouter()
-router.register(r'members', MemberViewSet)
